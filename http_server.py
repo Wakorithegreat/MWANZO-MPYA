0f006f35041aa822dfb944a4882457d666c600e5
Wakorithegreat <jkwakori@gmail.com>
2026-06-10 14:44:07 +0300

    Python HTTP server with GET and POST request additional features on the way

---
+++ http_server.py
@@ -0,0 +1,28 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import time 
+
+HOST = "192.168.100.49"
+PORT = 9999
+
+class NeuralHTTP(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        
+        self.wfile.write(bytes("<html><body><h1>ELENDELEA IVO! IVO!</h1></body></html>", "utf-8"))
+    
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        
+        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+        self.wfile.write(bytes('{"time": "'+ date + '"}', "utf-8"))
+        
+server = HTTPServer((HOST, PORT), NeuralHTTP)
+print("Server now running...")
+server.serve_forever()
+server.serve_close()
+
+print("Server stopped")
